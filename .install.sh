dab55fbc93b21f66c4444829a147190ea6fd8eba
Kensuke Watanabe <watanabe_k@jp.fujitsu.com>
2016-05-09 14:43:32 +0900

    自動環境設定スクリプトの追加
    usage:
        $ bash .install.sh

---
+++ .install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+APP_NAME=education
+CUR_DIR=`pwd`
+PRIORITY_NO=002
+
+echo "##### APP_NAME=${APP_NAME}"
+echo "##### CUR_DIR=${CUR_DIR}"
+echo "##### PRIORITY_NO=${PRIORITY_NO}"
+
+if [ ! -e ${APP_NAME}.apache.conf ] ; then
+    echo
+    echo "##### Setting Apache2..."
+    echo
+    cp .apache.conf.base ${APP_NAME}.apache.conf
+    sed -i "s|{{APP_NAME}}|${APP_NAME}|g" ${APP_NAME}.apache.conf
+    sed -i "s|{{CUR_DIR}}|${CUR_DIR}|g" ${APP_NAME}.apache.conf
+    sudo ln -s ${CUR_DIR}/${APP_NAME}.apache.conf /etc/apache2/sites-available/${PRIORITY_NO}-${APP_NAME}.apache.conf
+    sudo a2ensite ${PRIORITY_NO}-${APP_NAME}.apache.conf
+    sudo service apache2 restart
+fi
+
+if [ ! -e public/.htaccess ] ; then
+    echo
+    echo "##### Creating .htaccess file..."
+    echo
+    cp public/.htaccess.base public/.htaccess
+    sed -i "s|{{APP_NAME}}|${APP_NAME}|g" public/.htaccess
+fi
+
+if [ ! -e composer.lock ] ; then
+    echo
+    echo "##### composer install..."
+    echo
+    composer install
+else
+    echo
+    echo "##### composer update..."
+    echo
+    composer update
+fi
+
+if [ ! -e node_modules ] ; then
+    echo
+    echo "##### npm install..."
+    echo
+    npm install
+else
+    echo
+    echo "##### npm update..."
+    echo
+    npm update
+fi
+
+if [ ! -e .env ] ; then
+    echo
+    echo "##### Create environment file..."
+    echo
+    cp .env.example .env
+    php ./artisan key:generate
+fi
+
+echo
+echo "##### Create CSS..."
+echo
+node_modules/gulp/bin/gulp.js
