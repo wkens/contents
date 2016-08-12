2929a9fd4267939346001a158b79a2a512695eb2
Kensuke WATANABE <watanabe_k@jp.fujitsu.com>
2016-08-12 17:37:44 +0900

    アイコンの登録

--- resources/views/adgenda.blade.php
+++ resources/views/adgenda.blade.php
@@ -1,10 +1,10 @@
 {{-- resources/views/template.blade.phpに保存 --}}
 @section('adgenda')
-<div class="adgenda-icon">
-←
-</div>
+<div class="adgenda-icon right-arrow"></div>
 <div class="adgenda-list">
 {!! $adgenda or ''!!}
-aaaaaa
 </div>
+
+<script type="text/javascript">
+</script>
 @show
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,8 +1,6 @@
 {{-- resources/views/template.blade.phpに保存 --}}
 @section('menu')
-<div class="menu-icon">
-←
-</div>
+<div class="menu-icon left-arrow"></div>
 <div class="note-list">
 @foreach($noteDB as $note)
 <a href="{{url('note/'.$note->id)}}">{{$note->name}}</a></br>
