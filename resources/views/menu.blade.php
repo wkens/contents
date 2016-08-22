89f492a9354e21624cb887e3e835754e5a5a0988
Kensuke WATANABE <watanabe_k@jp.fujitsu.com>
2016-08-22 13:05:08 +0900

    メニューVIEW変更

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,7 +1,7 @@
 {{-- resources/views/template.blade.phpに保存 --}}
 @section('menu')
-<div class="menu-icon left-arrow"></div>
-<div class="note-list">
+<div id="menu-icon" class="menu-icon left-arrow" onclick="toggleMenu('#menu-icon','left-arrow','right-arrow','#menu-list');return false;"></div>
+<div id="menu-list" class="note-list">
 @foreach($noteDB as $note)
 <a href="{{url('note/'.$note->id)}}">{{$note->name}}</a></br>
 @endforeach
